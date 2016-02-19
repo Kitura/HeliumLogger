94ba5ca1f5dfe2081592a8a40cedb40f1833150a
rolivieri <roliv@us.ibm.com>
2016-02-19 15:05:51 -0600

    Updates dependency version numbers.

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
             dependencies: [.Target(name: "HeliumLogger")])],
 
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0),
+        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,2,0)..<Version(0,3,0)),
         //.Package(url: "git@github.com:IBM-Swift/LoggerAPI.git", majorVersion: 0)
     ]
 )
