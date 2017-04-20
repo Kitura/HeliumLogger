5837ac6232a2961018e760c3b58708d911594433
Quan Vo <quan.vo@ibm.com>
2017-04-20 12:50:50 -0500

    Add test for Mac vs Linux test count. Randomize test order on Linux. … (#48)

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -15,10 +15,35 @@
  **/
 
 import XCTest
-
+import Glibc
 @testable import HeliumLoggerTests
 
+srand(UInt32(time(nil)))
+
+// http://stackoverflow.com/questions/24026510/how-do-i-shuffle-an-array-in-swift
+extension MutableCollection where Indices.Iterator.Element == Index {
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+
+        for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            let d: IndexDistance = numericCast(random() % numericCast(unshuffledCount))
+            guard d != 0 else { continue }
+            let i = index(firstUnshuffled, offsetBy: d)
+            swap(&self[firstUnshuffled], &self[i])
+        }
+    }
+}
+
+extension Sequence {
+    func shuffled() -> [Iterator.Element] {
+        var result = Array(self)
+        result.shuffle()
+        return result
+    }
+}
+
 XCTMain([
-    testCase(TestLogger.allTests),
-    testCase(TestStreamLogger.allTests),
-    ])
+    testCase(TestLogger.allTests.shuffled()),
+    testCase(TestStreamLogger.allTests.shuffled()),
+    ].shuffled())
